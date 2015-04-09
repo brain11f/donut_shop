af75eed25c20946cb88ebca2fe589c4b0e6272b5
Brian Finck <brian@brianfinck.com>
2015-04-08 21:19:26 -0700

    Working CSS

--- donutshop.js
+++ donutshop.js
@@ -1,75 +1,58 @@
-(function() {
-    var Locations = function(min, max, avg, name, hoursOfOperations) {
-      this.min = min;
-      this.max = max;
-      this.avg = avg;
+//(function() {
+
+    var shops = [];
+    var Locations = function(locationName, options) {
+      this.locationName = locationName;
+      this.min = options.min;
+      this.max = options.max;
+      this.avg = options.avg;
+      this.opens = options.opens || 700;
+      this.closes = options.closes || 1800;
+      this.hoursOpen = (this.closes - this.opens)/100;
       this.donutsHour = [];
-      this.name = name;
-      this.perDay = 0;
-      this.hoursOfOperations = hoursOfOperations;
-    };
+    }
+
+shops.push(new Locations ('Downtown', {min: 8, max: 43, avg: 4.5}))
+shops.push(new Locations ('Capitol Hill', {min: 4, max: 37, avg: 2}))
+shops.push(new Locations ('South Lake Union', {min: 9, max: 23, avg: 6.33}))
+shops.push(new Locations ('Wedgewood', {min: 2, max: 28, avg: 1.25}))
+shops.push(new Locations ('Ballard', {min: 8, max: 58, avg: 3.75}))
+
+    Locations.prototype.donutsRandom = function() {
+      var newCustomers = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+      return newCustomers;
+    }
 
     Locations.prototype.donutsPerHour = function() {
-      var newCustomers = Math.floor(Math.random() * this.max - this.min + 1) + this.min;
-      return Math.round(newCustomers * this.avg);
-    };
+      for (i = 0; i <= this.hoursOpen; i++) {
+        var amountSold = this.donutsRandom() * this.avg;
+        this.donutsHour.push(amountSold);
+        };
+    }
 
     Locations.prototype.donutsPerDay = function() {
-      for (i = 0; i <= this.hoursOfOperations; i++) {
-      var donutsHourTotal = this.donutsPerHour();
-      this.donutsHour.push(donutsHourTotal);
-      this.perDay += donutsHourTotal;
-      }
-    };
-
-    Locations.prototype.renderNewRow = function() {
-      var tableRef = document.getElementById('myTable');
-      var newRow   = tableRef.insertRow(tableRef.rows.length);
-      var newCell  = newRow.insertCell(0);
-      var newText  = document.createTextNode('new row')
-      newCell.appendChild(newText);
-        var tr = document.createElement('tr');
-      };
+    var all = 0;
+    for (var i = 0; i < this.donutsHour.length; i++) {
+    all += this.donutsHour[i];
+    }
+    return all;
+}
 
     Locations.prototype.render = function() {
-      var elTr = document.getElementById(this.name);
-      for (var i = 0; i <= this.donutsHour.length; i++) {
-        var el = document.createElement('td');
-        el.textContent = this.donutsHour[i];
-        elTr.appendChild(el);
-      }
-      el.textContent = this.perDay;
-      elTr.appendChild(el);
-    };
+      this.donutsPerHour();
+      var locationInfo = this.donutsHour.join([separator = '</td><td>'])
+      var newRow = document.createElement('tr');
+      newRow.innerHTML = '<th class="location">' + this.locationName + '</th><td>' + locationInfo + '</td><td class="daily-total">' + this.donutsPerDay() + '</td>';
+      var position = document.getElementById('myTable');
+      position.appendChild(newRow);
+      return newRow;
+    }
 
-      var Downtown = new Locations(8, 43, 4.5, 'Downtown', 11);
-      var CapitalHill = new Locations(4, 37, 2, 'CapitalHill', 11);
-      var SouthLakeUnion = new Locations(9, 23, 6.33, 'SouthLakeUnion', 11);
-      var Wedgewood = new Locations(2, 28, 1.25, 'Wedgewood', 11);
-      var Ballard = new Locations(8, 58, 3.75, 'Ballard', 11);
-      Downtown.donutsPerDay();
-      Downtown.render();
-      CapitalHill.donutsPerDay();
-      CapitalHill.render();
-      SouthLakeUnion.donutsPerDay();
-      SouthLakeUnion.render();
-      Wedgewood.donutsPerDay();
-      Wedgewood.render();
-      Ballard.donutsPerDay();
-      Ballard.render();
-
-      var submitButton = document.getElementById('submitlocation');
-
-      submitButton.addEventListener('click', function () {
-      //var loc = new Locations(event.target.name.value) //in the order of my constructer functions perametersdasdjsdk
-      //alert(loc.value);
-      //console.log(loc);
-      //loc.donutsPerDay();
-      //loc.renderNewRow();
-      //loc.render();
-      var loc = new Locations([document.getElementById('value1'),document.getElementById('value2'),document.getElementById('value3'),document.getElementById('value4'),document.getElementById('value5')]);
-      console(loc.value);
-   });
+    for (var i = 0; i < shops.length; i++) {
+  shops[i].render();
+};
 
 
-})();
+window.Locations.locationName = Locations;
+
+//})();
